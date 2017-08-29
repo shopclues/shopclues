86b8f21785255597b40181a7503b0d9a37ed2a91
shopclues <31447361+shopclues@users.noreply.github.com>
2017-08-29 21:27:09 +0530

    Add files via upload

---
+++ shopclues-api.php
@@ -0,0 +1,14 @@
+<?php
+//This is a template of this API and we are working on the code development part.
+function getCategoryList($token) {
+	//Returns the list of all categories in JSON Format
+}
+
+function getProductList($category) {
+	//This one takes a category as input and returns the list of products
+}
+
+function getProductDetails($SKU) {
+	//This one takes input as product SKU and returns in-depth details and specifications of the product.
+}
+?>
